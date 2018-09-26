2ece5aca09a677b4bd9ddbf93ae9a1db867dd48e
gareth <gareth@gareths-mac-mini.local>
2018-09-26 21:33:34 +0100

    Testing update and cache-flush via modman

---
+++ flush_cache.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once './app/Mage.php';
+umask(0);
+Mage::app('default');
+Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+try {
+    $allTypes = Mage::app()->useCache();
+    foreach($allTypes as $type => $value) {
+        Mage::app()->getCacheInstance()->cleanType($type);
+        Mage::dispatchEvent('adminhtml_cache_refresh_type', array('type' => $type));
+        echo "{$type} </br>";
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+?>
