2f5ac7be82e3558b159149359fe4b0dc61dbce7c
gareth <gareth@192.168.1.100>
2018-09-28 20:48:34 +0100

    flush_cache.php now takes the magento root directory as an argument

--- flush_cache.php
+++ flush_cache.php
@@ -1,7 +1,21 @@
 <?php
 
-require_once './app/Mage.php';
+$magentoDir = '.';
+$options = getopt("d:",array('mage-dir:'));
+if (!empty($options['mage-dir']))
+{
+	$magentoDir = $options['mage-dir'];
+}
+if (!empty($options['d']))
+{
+	$magentoDir = $options['d'];
+}
+
+require_once $magentoDir.'/app/Mage.php';
+
+// Any files created will have global read-write permissions
 umask(0);
+
 Mage::app('default');
 Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
 
