ad1f2be883004baced6feacb5290cca61bd28b76
gareth <gareth@gareths-mac-mini.local>
2019-06-07 12:01:04 +0100

    Partial fix for #19
    1) Mappings and Category objects now cached.
    2) No longer loads all attributes for each product
    3) Reduced complexity
    
    The real bug appears to be in Magento code as it tries to load and
    convert images. However, importing a CSV exported by our controller
    action appears to work fine.

--- app/code/local/Gareth/NaturesCupboard2/Model/Observer.php
+++ app/code/local/Gareth/NaturesCupboard2/Model/Observer.php
@@ -31,72 +31,86 @@ class Gareth_NaturesCupboard2_Model_Observer extends Varien_Object
 	private static $_storeGroupName = 'Natures Cupboard';
 	
 	/**
+	 * A cache of attribute_code to Category instance which denotes the
+	 * categories a product should be added to/removed from if the attribute
+	 * is true/false.
+	 * 
+	 * @var array
+	 */
+	private static $_mappings = null;
+	
+	/**
 	 * Returns an array of attribute code to category URL key which
 	 * represents the categories to which products should be added to if the
 	 * product has the specified attribute set to true.
 	 */
-	protected function getAttributeCodeToCategoryUrlKeyMapping()
+	protected function getAttributeCodeToCategoryMap()
 	{
-		$mappings = array();
-		$model = Mage::getModel('gareth_naturescupboard2/attribtocategorymapping');
-		$mappingCollection = $model->getCollection();
-		/* @var Gareth_NaturesCupboard2_Model_AttribToCategoryMapping $thisMapping */
-		foreach ($mappingCollection as $thisMapping)
+		if (is_null(self::$_mappings))
 		{
-			$attributeCode = $thisMapping->getAttributeCode();
-			$categoryUrlKey = $thisMapping->getCategoryUrlKey();
-			
-			$mappings[$attributeCode] = $categoryUrlKey;
+			self::$_mappings = array();
+			$model = Mage::getModel('gareth_naturescupboard2/attribtocategorymapping');
+			$mappingCollection = $model->getCollection();
+			/* @var Gareth_NaturesCupboard2_Model_AttribToCategoryMapping $thisMapping */
+			foreach ($mappingCollection as $thisMapping)
+			{
+				$attributeCode = $thisMapping->getAttributeCode();
+				$categoryUrlKey = $thisMapping->getCategoryUrlKey();
+				
+				/** @var Gareth_NaturesCupboard2_Helper_Lookup $lookup */
+				$lookup= Mage::helper('gareth_naturescupboard2/lookup');
+				$mappedCategory = $lookup->findCategoryByUrlKey(self::$_storeGroupName, $categoryUrlKey);
+				if (!empty($mappedCategory))
+				{
+					self::$_mappings[$attributeCode] = $mappedCategory;
+				}
+			}
+			// garbage collect: clearInstance() does not exist at runtime!
+			//$mappingCollection->clearInstance();
 		}
-		return $mappings;
+		return self::$_mappings;
 	}
 	
 	protected function setProductCategories($product)
 	{
-		$productName = $product->getName();
-		$mappings = $this->getAttributeCodeToCategoryUrlKeyMapping();
-		$product_attributes = $product->getAttributes();//self::$_attributeSetGroupName, false);
-
+		// the categories the product is (currently) in
 		$categoryIds = $product->getCategoryIds();
-		/* @var Mage_Catalog_Model_Resource_Eav_Attribute $thisAttribute */
-		foreach ($product_attributes as $thisAttribute)
+		
+		$mappings = $this->getAttributeCodeToCategoryMap();
+		/* @var Mage_Catalog_Model_Category $mappedCategory */
+		foreach ($mappings as $attributeCode => $mappedCategory)
 		{
-			$thisAttributeCode = $thisAttribute->getAttributeCode();
-			$thisAttributeIsMapped = array_key_exists($thisAttributeCode, $mappings);
-			if ($thisAttributeIsMapped)
+			$attributeValue = $product->getData($attributeCode);
+			if (!is_null($attributeValue))
 			{
-				$categoryUrlKey = $mappings[$thisAttributeCode];			
-				/** @var Gareth_NaturesCupboard2_Helper_Lookup $lookup */
-				$lookup= Mage::helper('gareth_naturescupboard2/lookup');
-				/** @var Mage_Catalog_Model_Category $categoryToAddTo */
-				$mappedCategory = $lookup->findCategoryByUrlKey(self::$_storeGroupName, $categoryUrlKey);
-				$mappedCategoryId = $mappedCategory->getId();
-				
-				if (!is_null($mappedCategory))
+				// this product has this attribute
+				if ($attributeValue)
 				{
-					$thisAttributeIsTrue = $product->getData($thisAttributeCode);
-					if ($thisAttributeIsTrue)
-					{
-						// add the mapped category and all ancestors to this product
-						$categoryIds[] = $mappedCategoryId;
-						
-						$parentCategoryIds = $mappedCategory->getParentIds();
-						$categoryIds = array_merge($categoryIds, $parentCategoryIds);
-					}
-					else
-					{
-						// remove the mapped category and all descendents from this product
-						// getAllChilren returns mappedCategory ID also. true = as array
-						$childCategoryIdsToRemove = $mappedCategory->getAllChildren(true);
-						// returns $array1 having removed all elements from $array2
-						$categoryIds =array_diff($categoryIds, $childCategoryIdsToRemove);
-					}
+					// add the mapped category and all ancestors to this product
+					$categoryIds[] = $mappedCategory->getId();
+					
+					$parentCategoryIds = $mappedCategory->getParentIds();
+					$categoryIds = array_merge($categoryIds, $parentCategoryIds);
 				}
+				else
+				{
+					// remove the mapped category and all descendents from this product
+					// getAllChilren returns mappedCategory ID also. true = as array
+					$childCategoryIdsToRemove = $mappedCategory->getAllChildren(true);
+					// returns $array1 having removed all elements from $array2
+					$categoryIds =array_diff($categoryIds, $childCategoryIdsToRemove);
+				}
+			}
+			else
+			{
+				// this product does not have this attribute - ignore
 			}
 		}
 		
 		$categoryIds = array_unique($categoryIds);
 		$product->setCategoryIds($categoryIds);
+		
+		$productName = $product->getName();
 		Mage::log('Product '.$productName.' re-assigned categories: '.implode(',',$categoryIds), null, 'gareth.log');
 	}
 	
@@ -104,6 +118,7 @@ class Gareth_NaturesCupboard2_Model_Observer extends Varien_Object
 	 * Function called the catalog_product_save_before
 	 * observer configured in config.xml
 	 * 
+	 * @param Varien_Event_Observer $observer
 	 */
 	public function setCategoriesOnProduct($observer)
 	{
